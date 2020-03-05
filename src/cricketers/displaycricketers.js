edea10726d9f31020b8c83b028307060b75424d6
Yashwanth  Thota <Yashwanth.Thota@ggktech.com>
2020-03-05 15:51:23 +0530

    Added material components

--- src/cricketers/displaycricketers.js
+++ src/cricketers/displaycricketers.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteCricketer } from './cricketer.action';
-
+import { Container } from '@material-ui/core'
 export default function Displaycricketers() {
     let cricketers = useSelector((state) => state.cricketerdata.cricketers);
     const [selected, setCheckbox] = useState(false)
@@ -9,22 +9,23 @@ export default function Displaycricketers() {
     const dispatch = useDispatch();
 
     const handleDelete = (cricketer) => {
-        ;
         dispatch(deleteCricketer(cricketer));
     }
 
     return (
-        <div >
+        <Container maxWidth="false">
             {cricketers.length > 0 ? <h1>Cricketers list</h1> : <h1>Cricketers list is empty</h1>}
             {user.login && <p><input type="checkbox" name="isChecked" onChange={() => setCheckbox(!selected)} /> Display your favourites</p>}
-            {cricketers.filter(cricketer => selected ? cricketer.userId == user.logindata.id : true).map(cricketer => {
-                return <div style={{ textAlign: "left" }} key={cricketer.id}>
-                    <p>Name : <b>{cricketer.name}</b></p>
-                    <p>Country : <b>{cricketer.country}</b></p>
-                    {selected && <button onClick={() => handleDelete(cricketer)}>Delete cricketer</button>}
-                    <br />
-                </div>
-            })}
-        </div>
+            <Container maxWidth="false">
+                {cricketers.filter(cricketer => selected ? cricketer.userId === user.logindata.id : true).map(cricketer => {
+                    return <div style={{ textAlign: "left" }} key={cricketer.id}>
+                        <p>Name : <b>{cricketer.name}</b></p>
+                        <p>Country : <b>{cricketer.country}</b></p>
+                        {selected && <button onClick={() => handleDelete(cricketer)}>Delete cricketer</button>}
+                        <br />
+                    </div>
+                })}
+            </Container>
+        </Container>
     )
 }
